28e3c28a8a0ab6bd7f1319d16781cd436b44eb1f
wengxyu1030 <115277834+alinealinealine@users.noreply.github.com>
2023-07-31 17:52:45 -0400

    update other setups

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,39 +14,21 @@ export const Footer: FC = () => {
           target="_blank"
           rel="noreferrer"
         >
-          Mckay Wrigley
+          Aline
         </a>
-        based on the essays of
+        based on the songs of
         <a
           className="hover:opacity-50 ml-1"
           href="https://twitter.com/paulg"
           target="_blank"
           rel="noreferrer"
         >
-          Paul Graham
+          My Little Airport
         </a>
         .
       </div>
 
-      <div className="flex space-x-4">
-        <a
-          className="flex items-center hover:opacity-50"
-          href="https://twitter.com/mckaywrigley"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <IconBrandTwitter size={24} />
-        </a>
-
-        <a
-          className="flex items-center hover:opacity-50"
-          href="https://github.com/mckaywrigley/paul-graham-gpt"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <IconBrandGithub size={24} />
-        </a>
-      </div>
+      
     </div>
   );
 };
